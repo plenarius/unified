0a199295c8c3f3131717dff47a4c98bc26671462
plenarius <plenarius@gmail.com>
2019-11-02 17:53:55 -0400

    Fix merge glitches

--- NWNXLib/Hooking/FunctionHook.cpp
+++ NWNXLib/Hooking/FunctionHook.cpp
@@ -1,6 +1,6 @@
 #include "Hooking/FunctionHook.hpp"
 #include "Assert.hpp"
-#include "External/subhook/subhook.h
+#include "External/subhook/subhook.h"
 #include <Zycore/Types.h>
 #include <Zydis/Zydis.h>
 #include <Zycore/Status.h>
